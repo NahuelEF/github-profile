cabc1f61af0061250f7c864cc4ef8333beff6850
NahuelEF <nahuelfedele@gmail.com>
2024-01-25 01:29:25 -0300

    add: font family and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Be Vietnam Pro"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      'lightest-blue': '#CDD5E0',
+      'light-grayish-blue': '#4A5567',
+      'grayish-blue': '#364153',
+      'dark-grayish-blue': '#20293A',
+      'light-blue': '#3662E3',
+      'dark-blue-gray': '#1D1B48',
+      'darkest-blue': '#111729',
+    },
   },
   plugins: [],
 };
