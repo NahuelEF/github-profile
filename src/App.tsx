cf19f042603b793b3e13b3e1a4d21288bb3c9f48
NahuelEF <nahuelfedele@gmail.com>
2024-02-02 23:29:49 -0300

    add: all components in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Suspense, useState } from 'react';
+import { RepoDetails, Search, UserDetails } from './components';
+import { useFetchData } from './hooks/useFetchData';
+import { User } from './types/api';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [userName, setUserName] = useState('github');
+  const { data, error } = useFetchData(userName);
+
+  const handleSearchUser = (name: string): void => {
+    setUserName(name);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
+      <header className="flex min-h-60 items-start justify-center bg-[url(./assets/hero-image-github-profile.png)] bg-cover bg-center bg-no-repeat">
+        <Search onSearch={handleSearchUser} />
+      </header>
+      <main className="container mx-auto px-8 text-lightest-blue xl:px-28">
+        {error ? (
+          <p className="text-center">{error}</p>
+        ) : (
+          <Suspense fallback={<p>Loading user data...</p>}>
+            <section className="w-full">
+              <UserDetails user={data?.user as User} />
+            </section>
+            <Suspense fallback={<p>Loading repository data...</p>}>
+              <section className="w-full">
+                <div className="my-7 grid grid-cols-auto-fill items-start gap-8">
+                  {data?.repositories.slice(0, 4).map((repository) => {
+                    return <RepoDetails repository={repository} key={repository.id} />;
+                  })}
+                </div>
+                <p className="mb-5 text-center">View all respositories</p>
+              </section>
+            </Suspense>
+          </Suspense>
+        )}
+      </main>
     </>
   );
-}
+};
 
 export default App;
